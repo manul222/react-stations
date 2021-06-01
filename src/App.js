916728321a8b892f0bc3885f72d63c59714b10de
滝上耕平 <takigamikouhei@takigamikouheinoMacBook-Air.local>
2021-06-01 21:47:04 +0900

    station3 crear

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import './App.css'
 export const App = () => {
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>manulネコ</header>
+      <h2>manulネコ言うてんのに犬やんけ！</h2>
+      <img src="https://images.dog.ceo/breeds/akita/Japaneseakita.jpg" alt="ランダムな犬の画像"></img>
     </div>
   )
 }
